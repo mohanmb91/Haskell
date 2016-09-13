c1a8682deb8862e3ac1da0da6d006efb553a4b56
mohanmb91 <mohankumarmbuniv@gmail.com>
2016-09-12 17:29:18 -0700

    project 4 latest code

--- Project4.hs
+++ Project4.hs
@@ -1,55 +1,24 @@
--- 																		TAKING Int AS AN PARAMETER
-
-validityCheckSum :: (Integral a) => [a] -> a
+validityCheckSum :: [Integer] -> Integer
 validityCheckSum [] = 0
 validityCheckSum [x] = x
 validityCheckSum (x:y:xs) = digitSum (y * 2) + x + validityCheckSum xs
---validityCheckSum (x:y:xs) = [x, y*2] ++ validityCheckSum xs 
 
+digitSum :: Integer -> Integer
+digitSum n = div + mod
+    where (mod,div) = modulousDiv n
 
-digitSum :: (Integral a) => a -> a
-digitSum n = n `div` 10 + n `mod` 10 
-             
-isValidCard :: (Integral a) => a -> Bool
+isValidCard :: String -> Bool
 isValidCard x
-    | lengthCheck' x == 16 = validityCheckSum(convertIntoIntList x) `mod` 10 == 0
+    | length x == 16 = (validityCheckSum . convertIntoIntListAndReverse . convertStringIntoInteger) x `mod` 10 == 0
+    | otherwise = error "Debit or Credit card should contain 16 digit"
 
-convertIntoIntList :: (Integral a ) => a -> [a]
-convertIntoIntList 0 = []
-convertIntoIntList x = [x `mod` 10] ++ convertIntoIntList (x `div` 10) 
+convertStringIntoInteger :: String -> Integer
+convertStringIntoInteger x = read x :: Integer
 
-lengthCheck' ::(Integral a) => a -> a
-lengthCheck' 0 = 0
-lengthCheck' x = 1 + lengthCheck' (x `div` 10)
+modulousDiv :: Integer -> (Integer,Integer)
+modulousDiv n = (n `mod` 10, n `div` 10)
 
--- 																		TAKING STRING AS AN PARAMETER
-
-
---validityCheckSum :: (Integral a) => [a] -> a
---validityCheckSum [] = 0
---validityCheckSum [x] = x
---validityCheckSum (x:y:xs) = digitSum (y * 2) + x + validityCheckSum xs
-----validityCheckSum (x:y:xs) = [x, y*2] ++ validityCheckSum xs 
-
-
---digitSum :: (Integral a) => a -> a
---digitSum n = n `div` 10 + n `mod` 10 
-
-
---isValidCard :: String -> Bool
---isValidCard "" = False
---isValidCard (x:xs)
---    | length (x:xs) == 16 = validityCheckSum (convertIntoIntList (convertStringIntoInteger (x:xs))) `mod` 10 == 0
-
---convertStringIntoInteger :: String -> Integer
---convertStringIntoInteger x = read x :: Integer
-
-
---convertIntoIntList :: Integer -> [Integer]
---convertIntoIntList 0 = []
---convertIntoIntList x = [x `mod` 10] ++ convertIntoIntList (x `div` 10) 
-
-
--- 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6  <== Input
--- 
--- 2 2 6 4 1 6 5 8 9 0 2 2 6 4 1 6  <== Excepted Output
+convertIntoIntListAndReverse :: Integer -> [Integer]
+convertIntoIntListAndReverse 0 = []
+convertIntoIntListAndReverse x = [mod] ++ convertIntoIntListAndReverse (div)
+    where (mod,div) = modulousDiv x
